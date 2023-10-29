376e9c411db544f3c27ad581d339a0a933c7f4dc
VenuraKS25 <vks250102@gmail.com>
2023-10-29 18:49:18 +0530

    backend complete to fetch products. Alos implemented the amozin s3 buckent. Moving to frontend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,57 @@
-const express = require('express')
-const cors = require('cors')
+import dotenv from "dotenv";
+dotenv.config();
 
-const app = express()
-app.use(cors())
+import express from "express";
+import cors from "cors";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { getVariants } from "./modules/varinats.js";
 
-app.get("/", (req, res) => {
-    res.status(200).send({message : "Hello World"})
-})
+const app = express();
+app.use(cors());
 
-app.listen(5000, () => {
-    console.log("App listening on port 5000")
-})
+const s3BucketName = process.env.S3_BUCKET_NAME;
+const s3BucketRegion = process.env.S3_BUCKET_REGION;
+const s3AccessKey = process.env.S3_ACCESS_KEY;
+const s3SecretAccessKey = process.env.S3_SECRET_ACCESS_KEY;
+
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: s3AccessKey,
+    secretAccessKey: s3SecretAccessKey,
+  },
+  region: s3BucketRegion,
+});
+
+app.get("/api/products", async (req, res) => {
+  try {
+    let products = await getVariants();
+    let outputProducts = [];
+
+    for (let product of products) {
+      let getObjectParams = {
+        Bucket: s3BucketName,
+        Key: product.image,
+      };
+
+      const command = new GetObjectCommand(getObjectParams);
+      const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+
+      outputProducts.push({
+        ...product,
+        imageUrl: url,
+      });
+    }
+
+    res.status(200).send(outputProducts);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: "Error fetching products" });
+  }
+});
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
---
+++ backend/modules/varinats.js
@@ -0,0 +1,28 @@
+import {pool} from './database.js'
+
+export const getVariants = async () => {
+    let results = [];
+    try {
+        const [rows] = await pool.query(
+            `select 
+                p.product_id, 
+                p.title, 
+                v.variant_id, 
+                v.price, 
+                p.weight,  
+                p.category_id, 
+                v.image, 
+                v.details, 
+                pr.detailed_description 
+                from product as p 
+                join variant as v 
+                on p.product_id = v.product_id 
+                join property as pr 
+                on v.variant_id = pr.variant_id`);
+        results = rows;
+    } catch (err) {
+        console.error("Error querying the database:", err);
+    } finally {
+        return results;
+    }
+};
