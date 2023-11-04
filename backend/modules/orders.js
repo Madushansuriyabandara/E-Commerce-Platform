76a8aebf546e3ac286532f6513b871bb95926cd8
VenuraKS25 <vks250102@gmail.com>
2023-11-04 14:08:19 +0530

    Implemented methods for getting "years(report generation)" and a methods for getting order details and order items

--- backend/modules/orders.js
+++ backend/modules/orders.js
@@ -18,3 +18,52 @@ export async function insertOrder(orderData) {
       throw error;
     }
   }
+
+  export const getDistinctOrderYears = async () => {
+    const [results] = await pool.query('SELECT DISTINCT YEAR(order_date) AS year FROM orders');
+    return results.map(row => row.year);
+  };
+
+  export const getOrderDetails = async () => {
+    const query = `
+      SELECT 
+        o.order_id, 
+        CONCAT(u.first_name, ' ', u.last_name) AS name, 
+        u.phone_no, 
+        o.total_amount, 
+        o.delivery_estimate 
+      FROM 
+        orders AS o
+      JOIN 
+        user AS u ON o.customer_id = u.user_id
+      ORDER BY o.order_id;
+    `;
+  
+    try {
+      const [rows] = await pool.query(query);
+      return rows;
+    } catch (error) {
+      console.error('Error executing query', error.stack);
+      throw error;
+    }
+  };
+
+  // Assuming 'pool' is your database connection pool
+export const getOrderItems = async (orderId) => {
+  const query = `
+    SELECT ci.product_id, ci.variant_id, p.title, v.details
+    FROM orders AS o
+    JOIN cart_items AS ci ON o.cart_id = ci.cart_id
+    JOIN product AS p ON ci.product_id = p.product_id
+    JOIN variant AS v ON ci.variant_id = v.variant_id
+    WHERE o.order_id = ?
+  `;
+
+  try {
+    const [rows] = await pool.execute(query, [orderId]);
+    return rows;
+  } catch (err) {
+    console.error(err);
+    throw new Error('Error querying the database');
+  }
+};
